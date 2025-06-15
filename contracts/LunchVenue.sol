3fe3b2c2b4e50ed6121e1f13d7ff6c4ce9a7f772
ethanwong3 <ethaneswanebban@gmail.com>
2025-06-15 21:24:53 +1000

    fixed bugs

--- contracts/LunchVenue.sol
+++ contracts/LunchVenue.sol
@@ -1,55 +1,63 @@
-// SPDX-License-Identifier: UNLICENSED
+/// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
 /// @title Contract to agree on the lunch venue
-/// @author Dilum Bandara, CSIRO’s Data61
+/// @author Dilum Bandara, CSIRO's Data61
 
-contract LunchVenue {
-
+contract LunchVenue{
+    
     struct Friend {
         string name;
-        bool voted;
+        bool voted; //Vote state
     }
-
+    
     struct Vote {
         address voterAddress;
         uint restaurant;
     }
 
-    mapping(uint => string) public restaurants; // restaurant number => name
-    mapping(address => Friend) public friends;  // address => Friend
-    mapping(uint => Vote) public votes;         // vote number => Vote
-    mapping(uint => uint) private _results;     // restaurant number => number of votes
-
+    mapping (uint => string) public restaurants; //List of restaurants (restaurant no, name)
+    mapping(address => Friend) public friends;  //List of friends (address, Friend)
     uint public numRestaurants = 0;
     uint public numFriends = 0;
     uint public numVotes = 0;
+    address public manager;                     //Contract manager
+    string public votedRestaurant = "";         //Where to have lunch
 
-    address public manager;
-    string public votedRestaurant = "";
-    bool public voteOpen = true;
+    mapping (uint => Vote) public votes;        //List of votes (vote no, Vote)
+    mapping (uint => uint) private _results;    //List of vote counts (restaurant no, no of votes)
+    bool public voteOpen = true;                //voting is open
 
-    /// @dev Set manager when contract starts
-    constructor() {
-        manager = msg.sender;
+    /**
+     * @dev Set manager when contract starts
+     */
+    constructor () {
+        manager = msg.sender;                   //Set contract creator as manager
     }
 
-    /// @notice Add a new restaurant
-    /// @dev To simplify, duplicate check is not done
-    /// @param name Restaurant name
-    /// @return Number of restaurants added so far
-    function addRestaurant(string memory name) public restricted returns (uint) {
+    /**
+     * @notice Add a new restaurant
+     * @dev To simplify the code, duplication of restaurants isn't checked
+     *
+     * @param name Restaurant name
+     * @return Number of restaurants added so far
+     */
+    function addRestaurant(string memory name) public restricted returns (uint){
         numRestaurants++;
         restaurants[numRestaurants] = name;
         return numRestaurants;
     }
 
-    /// @notice Add a new friend to voter list
-    /// @dev Duplicate check is not done
-    /// @param friendAddress Friend’s Ethereum address
-    /// @param name Friend’s name
-    /// @return Number of friends added so far
-    function addFriend(address friendAddress, string memory name) public restricted returns (uint) {
+    /**
+     * @notice Add a new friend to voter list
+     * @dev To simplify the code duplication of friends is not checked
+     *
+     * @param friendAddress Friend's account/address
+     * @param name Friend's name
+     * @return Number of friends added so far
+     */
+    function addFriend(address friendAddress, string memory name) public restricted returns (uint){
         Friend memory f;
         f.name = name;
         f.voted = false;
@@ -57,15 +65,19 @@ contract LunchVenue {
         numFriends++;
         return numFriends;
     }
-
-    /// @notice Vote for a restaurant
-    /// @dev Duplicate vote not checked
-    /// @param restaurant Restaurant number
-    /// @return validVote Whether vote is valid
-    function doVote(uint restaurant) public votingOpen returns (bool validVote) {
-        validVote = false;
-        if (bytes(friends[msg.sender].name).length != 0) { // Is friend valid?
-            if (bytes(restaurants[restaurant]).length != 0) { // Is restaurant valid?
+    
+    /** 
+     * @notice Vote for a restaurant
+     * @dev To simplify the code duplicate votes by a friend is not checked
+     *
+     * @param restaurant Restaurant number being voted
+     * @return validVote Is the vote valid? A valid vote should be from a registered 
+     * friend to a registered restaurant
+    */
+    function doVote(uint restaurant) public votingOpen returns (bool validVote){
+        validVote = false;                                  //Is the vote valid?
+        if (bytes(friends[msg.sender].name).length != 0) {  //Does friend exist?
+            if (bytes(restaurants[restaurant]).length != 0) {   //Does restaurant exist?
                 validVote = true;
                 friends[msg.sender].voted = true;
                 Vote memory v;
@@ -75,44 +87,48 @@ contract LunchVenue {
                 votes[numVotes] = v;
             }
         }
-
-        if (numVotes >= numFriends / 2 + 1) {
+        
+        if (numVotes >= numFriends/2 + 1) { //Quorum is met
             finalResult();
         }
-
         return validVote;
     }
 
-    /// @notice Determine winner restaurant
-    /// @dev If tie, winner depends on vote order
-    function finalResult() private {
+    /** 
+     * @notice Determine winner restaurant
+     * @dev If top 2 restaurants have the same no of votes, result depends on vote order
+    */
+    function finalResult() private{
         uint highestVotes = 0;
         uint highestRestaurant = 0;
-
-        for (uint i = 1; i <= numVotes; i++) {
+        
+        for (uint i = 1; i <= numVotes; i++){   //For each vote
             uint voteCount = 1;
-            if (_results[votes[i].restaurant] > 0) {
+            if(_results[votes[i].restaurant] > 0) { // Already start counting
                 voteCount += _results[votes[i].restaurant];
             }
             _results[votes[i].restaurant] = voteCount;
-
-            if (voteCount > highestVotes) {
+        
+            if (voteCount > highestVotes){ // New winner
                 highestVotes = voteCount;
                 highestRestaurant = votes[i].restaurant;
             }
         }
-
-        votedRestaurant = restaurants[highestRestaurant];
-        voteOpen = false;
+        votedRestaurant = restaurants[highestRestaurant];   //Chosen restaurant
+        voteOpen = false;                                   //Voting is now closed
     }
-
-    /// @notice Only the manager can call
+    
+    /** 
+     * @notice Only the manager can do
+     */
     modifier restricted() {
-        require(msg.sender == manager, "Can only be executed by the manager");
+        require (msg.sender == manager, "Can only be executed by the manager");
         _;
     }
-
-    /// @notice Only when voting is still open
+    
+    /**
+     * @notice Only when voting is still open
+     */
     modifier votingOpen() {
         require(voteOpen == true, "Can vote only while voting is open.");
         _;
--- tests/LunchVenue_test.sol
+++ tests/LunchVenue_test.sol
@@ -1,27 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.0 <0.9.0;
+        
+pragma solidity >=0.8.00 <0.9.0;
 
 // This import is automatically injected by Remix
-import "remix_tests.sol";
+import "remix_tests.sol"; 
 
 // This import is required to use custom transaction context
-// Although it may fail compilation in ‘Solidity Compiler’ plugin
-// But it will work fine in ‘Solidity Unit Testing’ plugin
+// Although it may fail compilation in 'Solidity Compiler' plugin
+// But it will work fine in 'Solidity Unit Testing' plugin
 import "remix_accounts.sol";
 import "../contracts/LunchVenue.sol";
 
-// File name has to end with ’_test.sol’, this file can contain more than one testSuite contracts
-// Inherit ‘LunchVenue’ contract
+// File name has to end with '_test.sol', this file can contain more than one testSuite contracts
+/// Inherit 'LunchVenue' contract
 contract LunchVenueTest is LunchVenue {
-    // Variables used to emulate different accounts
-    address acc0;
+    
+    // Variables used to emulate different accounts  
+    address acc0;   
     address acc1;
     address acc2;
     address acc3;
     address acc4;
 
-    // ‘beforeAll’ runs before all other tests
-    /// More special functions are: ’beforeEach’, ’beforeAll’, ’afterEach’ & ’afterAll’
+    /// 'beforeAll' runs before all other tests
+    /// More special functions are: 'beforeEach', 'beforeAll', 'afterEach' & 'afterAll'
     function beforeAll() public {
         // Initiate account variables
         acc0 = TestsAccounts.getAccount(0);
@@ -31,101 +33,104 @@ contract LunchVenueTest is LunchVenue {
         acc4 = TestsAccounts.getAccount(4);
     }
 
-    // Check manager
+    /// Check manager
     /// account-0 is the default account that deploy contract, so it should be the manager (i.e., acc0)
     function managerTest() public {
-        Assert.equal(manager, acc0, "Manager should be acc0");
+        Assert.equal(manager, acc0, 'Manager should be acc0');
     }
-
-    // Add restaurant as manager
-    /// When msg.sender isn’t specified, default account (i.e., account-0) is the sender
+    
+    /// Add restuarant as manager
+    /// When msg.sender isn't specified, default account (i.e., account-0) is the sender
     function setRestaurant() public {
-        Assert.equal(addRestaurant("Courtyard Cafe"), 1, "Should be equal to 1");
-        Assert.equal(addRestaurant("Uni Cafe"), 2, "Should be equal to 2");
+        Assert.equal(addRestaurant('Courtyard Cafe'), 1, 'Should be equal to 1');
+        Assert.equal(addRestaurant('Uni Cafe'), 2, 'Should be equal to 2');
     }
-
-    // Try to add a restaurant as a user other than manager
-    // #sender: account-1
+    
+    /// Try to add a restaurant as a user other than manager. This should fail
+    /// #sender: account-1
     function setRestaurantFailure() public {
         // Try to catch reason for failure using try-catch . When using
-        // try-catch we need ’this’ keyword to make function call external
-        try this.addRestaurant("Atomic Cafe") returns (uint v) {
-            Assert.notEqual(v, 3, "Method execution did not fail");
+        // try-catch we need 'this' keyword to make function call external
+        try this.addRestaurant('Atomic Cafe') returns (uint v){
+            Assert.notEqual(v, 3, 'Method execution did not fail');
         } catch Error(string memory reason) {
-            Assert.equal(reason, "Can only be executed by the manager", "Failed with unexpected reason");
-        } catch Panic(uint) {
-            Assert.ok(false, "Failed unexpected with error code");
-        } catch (bytes memory) {
-            Assert.ok(false, "Failed unexpected");
-        }
-    }
-
-    // Set friends as account-0
-    /// #sender doesn’t need to be specified explicitly for account-0
-    function setFriend() public {
-        Assert.equal(addFriend(acc0, "Alice"), 1, "Should be equal to 1");
-        Assert.equal(addFriend(acc1, "Bob"), 2, "Should be equal to 2");
-        Assert.equal(addFriend(acc2, "Charlie"), 3, "Should be equal to 3");
-        Assert.equal(addFriend(acc3, "Eve"), 4, "Should be equal to 4");
-    }
-
-    // Try adding friend as a user other than manager
-    function setFriendFailure() public {
-        try this.addFriend(acc4, "Daniels") returns (uint f) {
-            Assert.notEqual(f, 5, "Method execution did not fail");
-        } catch Error(string memory reason) {
-            Assert.equal(reason, "Can only be executed by the manager", "Failed with unexpected reason");
-        } catch Panic(uint) {
-            Assert.ok(false, "Failed unexpected with error code");
-        } catch (bytes memory) {
-            Assert.ok(false, "Failed unexpected");
+            // Compare failure reason, check if it is as expected
+            Assert.equal(reason, 'Can only be executed by the manager', 'Failed with unexpected reason');
+        } catch Panic(uint /* errorCode */) { // In case of a panic
+            Assert.ok(false, 'Failed unexpected with error code');
+        } catch (bytes memory /*lowLevelData*/) {
+            Assert.ok(false, 'Failed unexpected');
         }
     }
     
-    // Vote as Bob
-    // #sender: account-1
+    /// Set friends as account-0
+    /// #sender doesn't need to be specified explicitly for account-0
+    function setFriend() public {
+       Assert.equal(addFriend(acc0, 'Alice'), 1, 'Should be equal to 1');
+       Assert.equal(addFriend(acc1, 'Bob'), 2, 'Should be equal to 2');
+       Assert.equal(addFriend(acc2, 'Charlie'), 3, 'Should be equal to 3');
+       Assert.equal(addFriend(acc3, 'Eve'), 4, 'Should be equal to 4');
+    }
+    
+    /// Try adding friend as a user other than manager. This should fail
+    function setFriendFailure() public {
+        try this.addFriend(acc4, 'Daniels') returns (uint f) {
+            Assert.notEqual(f, 5, 'Method execution did not fail');
+        } catch Error(string memory reason) { // In case revert() called
+            // Compare failure reason, check if it is as expected
+            Assert.equal(reason, 'Can only be executed by the manager', 'Failed with unexpected reason');
+        } catch Panic( uint /* errorCode */) { // In case of a panic
+            Assert.ok(false , 'Failed unexpected with error code');
+        } catch (bytes memory /*lowLevelData*/) {
+            Assert.ok(false, 'Failed unexpected');
+        }
+    }
+    
+    /// Vote as Bob (acc1)
+    /// #sender: account-1
     function vote() public {
         Assert.ok(doVote(2), "Voting result should be true");
     }
 
-    // Vote as Charlie
-    // #sender: account-2
+    /// Vote as Charlie
+    /// #sender: account-2
     function vote2() public {
         Assert.ok(doVote(1), "Voting result should be true");
     }
-
-    // Try voting as a user not in the friends list
-    // #sender: account-4
+    
+	/// Try voting as a user not in the friends list. This should fail
+    /// #sender: account-4
     function voteFailure() public {
         Assert.equal(doVote(1), false, "Voting result should be false");
     }
 
-    // Vote as Eve
-    // #sender: account-3
+    /// Vote as Eve
+    /// #sender: account-3
     function vote3() public {
         Assert.ok(doVote(2), "Voting result should be true");
     }
 
-    // Verify lunch venue is set correctly
+    /// Verify lunch venue is set correctly
     function lunchVenueTest() public {
-        Assert.equal(votedRestaurant, "Uni Cafe", "Selected restaurant should be Uni Cafe");
+        Assert.equal(votedRestaurant, 'Uni Cafe', 'Selected restaurant should be Uni Cafe');
     }
-
-    // Verify voting is now closed
+    
+    /// Verify voting is now closed
     function voteOpenTest() public {
-        Assert.equal(voteOpen, false, "Voting should be closed");
+        Assert.equal(voteOpen, false, 'Voting should be closed');
     }
-
-    // Verify voting after vote closed. This should fail
+    
+    /// Verify voting after vote closed. This should fail
     function voteAfterClosedFailure() public {
         try this.doVote(1) returns (bool validVote) {
-            Assert.equal(validVote, true, "Method execution did not fail");
+            Assert.equal(validVote, true, 'Method execution did not fail');
         } catch Error(string memory reason) {
-            Assert.equal(reason, "Can vote only while voting is open.", "Failed with unexpected reason");
-        } catch Panic(uint) {
-            Assert.ok(false, "Failed unexpected with error code");
-        } catch (bytes memory) {
-            Assert.ok(false, "Failed unexpectedly");
+            // Compare failure reason, check if it is as expected
+            Assert.equal(reason, 'Can vote only while voting is open.', 'Failed with unexpected reason');
+        } catch Panic( uint /* errorCode */) { // In case of a panic
+            Assert.ok(false , 'Failed unexpected with error code');        
+        } catch (bytes memory /*lowLevelData*/) {
+            Assert.ok(false, 'Failed unexpectedly');
         }
     }
 }
